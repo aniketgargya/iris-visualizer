7b44e0dbe623c0164d093f42e89d7e890d6331a8
Aniket Gargya <gargya.aniket@gmail.com>
2020-05-18 14:21:33 -0500

    ignoring out/

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,15 +6,34 @@ const Index: FC<{}> = () => {
     const data: ChartData<Chart.ChartData> = {
         datasets: [
             {
-                label: 'Hey',
+                label: "Setosa",
                 data: irisFlowers.filter(({ species }) => species == "setosa").map(({ petal_length: x, petal_width: y }) => ({ x, y })),
-                pointBackgroundColor: 'green',
-                pointBorderColor: 'green'
+                pointBackgroundColor: "#2b00ff",
+                pointBorderColor: "#2b00ff",
+                backgroundColor: "#2b00ff"
+            },
+            {
+                label: "Versicolor",
+                data: irisFlowers.filter(({ species }) => species == "versicolor").map(({ petal_length: x, petal_width: y }) => ({ x, y })),
+                pointBackgroundColor: "#ff0022",
+                pointBorderColor: "#ff0022",
+                backgroundColor: "#ff0022"
+            },
+            {
+                label: "Virginica",
+                data: irisFlowers.filter(({ species }) => species == "virginica").map(({ petal_length: x, petal_width: y }) => ({ x, y })),
+                pointBackgroundColor: "#00de29",
+                pointBorderColor: "#00de29",
+                backgroundColor: "#00de29"
             }
         ]
     };
 
-    return <Scatter data={data} />
+    return (
+        <div className="w-1/2 m-auto">
+            <Scatter data={data} />
+        </div>
+    );
 };
 
 export default Index;
